60f1f9aeed017b78f9f00a520921deb584730438
tartakovskyi <vvt2001@ukr.net>
2021-02-04 16:59:49 +0200

    start

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,41 @@
-import React  from 'react';
+import React, {useEffect, useState}  from 'react'
 
 
 const App = () => {
 
-  fetch('lawyers.csv')
-  .then(response => response.text())
-  .then(data => console.log(data))
+	const [lawyers, setLawyers] = useState([])
 
-  return (
-    <h1>Test</h1>
-  )
+	useEffect(() => {
+		fetch('lawyers.csv')
+		.then(response => response.text())
+		.then(data => {
+			parseLawyers(data)
+		})
+	},[])
+
+	const parseLawyers = data => {
+		const lawyerStrings = data.split(/\r?\n|\r/)
+		let lawyersArr = []
+		const keys = lawyerStrings[0].split(';')
+		for (var i = 1; i < lawyerStrings.length; i++) {
+			const lawyerStringsItem = lawyerStrings[i].split(';')
+			let lawyer = {}
+			for (var j = 0; j < lawyerStringsItem.length; j++) {
+				lawyer[keys[j]] = lawyerStringsItem[j]
+			}
+
+			lawyersArr[i] = lawyer
+		}
+		setLawyers(lawyersArr)
+	}
+
+	
+
+	return (
+		<div className="container">
+			<h1>Test</h1>
+		</div>
+	)
 }
 
 export default App;
